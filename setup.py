d68ca76da138f2ae343edfd043ed09b0e6b87865
Ilya Semenov <semenov@inetss.com>
2016-09-20 17:54:44 +0700

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='django-sequential-pagination',
-	version='0.0.2',
+	version='0.0.3',
 	url='https://github.com/IlyaSemenov/django-sequential-pagination',
 	license='BSD',
 	author='Ilya Semenov',
